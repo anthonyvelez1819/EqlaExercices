b083bc9cdf57aa8d8ba6bf3eb09a43d7d8dd8129
Anthony Velez <josuevelez@outlook.es>
2023-11-22 14:23:37 +0100

    ajout exercice 7

--- php/exercice12.php
+++ php/exercice12.php
@@ -2,4 +2,13 @@
 echo "entrez un nombre:";
 $number1=fgets(STDIN);
 $number1=trim($number1);
-echo "entrez un "
+echo "est-il divisible par:";
+$number2=fgets(STDIN);
+$number2=trim($number2);
+$resultat= $number1%$number2;
+if ($resultat==0) {
+    echo "il est divisible par".$number2;
+} else {
+    echo "il n'est pas divisible";
+}
+?>
