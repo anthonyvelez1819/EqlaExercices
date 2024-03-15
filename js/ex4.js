b4304c7a38c4462c01d2be98877d0355f1558f4d
Anthony Velez <josuevelez@outlook.es>
2024-03-15 13:43:25 +0100

    ajout exercice

---
+++ js/ex4.js
@@ -0,0 +1,8 @@
+let prenom = "Johnny";
+let age = "55";
+let estMajeur = "true";
+let taille = "1.75";
+let poids = "75";
+let sexe = "M";
+console.log(`Je m'appelle ${prenom}, j'ai ${age} ans, je mesure ${taille}m, je pèse ${poids*2}kg et je suis de sexe '${sexe}'.`);
+console.log(`majeur:${estMajeur}`);
